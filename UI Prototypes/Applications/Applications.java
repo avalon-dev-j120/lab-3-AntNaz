992a6cdc4614a9cb91e82232f62568c2837098b7
Anton <Anton@192.168.0.100>
2019-12-03 13:30:11 +0300

    Задания 1, 2, 3.

---
+++ UI Prototypes/Applications/Applications.java
@@ -0,0 +1,10 @@
+
+package Applications;
+
+public class Applications {
+    
+    public static void main(String[] args) {
+        //new Windows.MainWindow(300, 450);
+        new Calculator.Calculator();
+    }
+}
